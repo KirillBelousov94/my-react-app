d225da8bebf71875596651184737696e0ed6ed26
Kirill Belousov <121androx121@gmail.com>
2026-06-08 11:08:33 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Мой лендинг</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h2>Главный блок</h2>
+      <p>Первый React-проект</p>
+    </section>
+  );
+}
+
+export default Hero;
